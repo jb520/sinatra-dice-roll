99ad3a725d960c2a495489da3fa3eb502031a975
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-02-22 20:42:48 +0000

    add links

--- dice.rb
+++ dice.rb
@@ -7,7 +7,13 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  "<h2>Roll the dice!</h2>
+    <ul>
+      <li><a href =\"/dice/2/6\">Roll two 6-sided dice</a></li>
+      <li><a href =\"/dice/2/10\">Roll two 10-sided dice</a></li>
+      <li><a href =\"/dice/1/20\">Roll one 20-sided die</a></li>
+      <li><a href =\"/dice/5/4\">Roll five 4-sided dice</a></li>
+    </ul>"
 end
 
 get("/zebra") do
